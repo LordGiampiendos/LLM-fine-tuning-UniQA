53829eee15229bec62e339b1469890334bdfd24d
Giampiero Barbaro <85945128+LordGiampiendos@users.noreply.github.com>
2025-03-08 14:00:51 +0100

    Add files via upload

---
+++ Metriche-LLM-10-GPT/Metriche_RAG.py
@@ -0,0 +1,63 @@
+from ragas.metrics import Faithfulness, AnswerCorrectness
+from ragas import evaluate
+import nest_asyncio
+import os
+from ragas.llms import LangchainLLMWrapper
+from langchain_openai import ChatOpenAI
+    
+nest_asyncio.apply()
+
+os.environ["OPENAI_API_KEY"] = "token"
+evaluator_llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-4o-mini"))
+
+index = [0, 1, 1156, 1158, 1162, 1168, 1170, 1184, 1238, 1241]
+
+import json
+from datasets import Dataset 
+
+# Carica i dataset di valutazione
+with open('/home/gbarbaro/Test/Inferenza_Fine_Tuning/Risultati_FT/output_metric_LLAMA_3.3_FT.json', 'r') as file:
+    data = json.load(file)
+
+j=0
+output = [data[j]['output_pairs']['output'][i] for i in index] 
+reference = [data[j]['output_pairs']['reference'][i] for i in index]
+
+with open('/home/gbarbaro/UniQA/lora_ft_reduced_chat/it_test.json', 'r') as file:
+    data_input = json.load(file)
+
+user_input = [data_input[i]["question"] for i in index]
+
+retrieved_contexts = []
+
+for i in index:
+    if "Documents" in data_input[i]["input"]: 
+        indice = data_input[i]["input"].find("Documents") 
+        retrieved_contexts.append(data_input[i]["input"][indice + len("Documents:\n"):].strip().split('\n')) 
+    else: retrieved_contexts.append([])
+
+# Converti output_pairs in un formato compatibile con Hugging Face Dataset 
+dataset_dict = {"user_input": user_input, "reference": reference, "response": output, "retrieved_contexts": retrieved_contexts} 
+hf_dataset = Dataset.from_dict(dataset_dict)
+
+metrics = [
+    AnswerCorrectness(llm = evaluator_llm),
+    Faithfulness(llm = evaluator_llm),
+]
+
+results = evaluate(dataset=hf_dataset, metrics=metrics, raise_exceptions=False)
+
+print(results)
+
+try:
+    with open('output_metric_RAG.json', 'r') as file:
+        output_metric_json = json.load(file)
+except FileNotFoundError:
+    output_metric_json = []
+except json.JSONDecodeError:
+    output_metric_json = []
+
+output_metric_json.append({"ac": results["answer_correctness"], "f": results["faithfulness"]})
+    
+with open('output_metric_RAG.json', 'w') as file:
+    json.dump(output_metric_json, file, indent=4)
