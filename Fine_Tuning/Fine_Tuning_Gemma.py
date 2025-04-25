ed715689d2369e5dd383f6ba53dcdcc399d74d85
Giampiero Barbaro <85945128+LordGiampiendos@users.noreply.github.com>
2025-04-25 21:35:18 +0200

    Update Fine_Tuning_Gemma.py

--- Fine_Tuning/Fine_Tuning_Gemma.py
+++ Fine_Tuning/Fine_Tuning_Gemma.py
@@ -1,9 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import LoraConfig
 
+# Token Hugging Face
 token = "token"
 model_name_or_path = "/path/LLM/gemma2-9b-inst"
 
+# PEFT Config
 peft_config = LoraConfig(
     r=16,
     lora_alpha=32,
@@ -13,6 +15,7 @@ peft_config = LoraConfig(
     task_type="CAUSAL_LM",
 )
 
+# Model Import
 model = AutoModelForCausalLM.from_pretrained(
     pretrained_model_name_or_path=model_name_or_path,
     token=token,
@@ -27,30 +30,34 @@ tokenizer.padding_side = 'right'
 
 import json
 
+# Train Dataset
 with open('/path/Dataset/it_train.json', 'r') as file:
     data = json.load(file)
     
 from sklearn.model_selection import train_test_split
 
+# Seed e Split Methods
 seed = 36
 train_data, validation_data = train_test_split(data, test_size=0.2, shuffle=True, random_state=seed)
 
+# Datasets Creation
 from datasets import Dataset, DatasetDict
 
 def convert_to_dict(data): 
     result = {k: [dic[k] for dic in data] for k in data[0]} 
     return result 
 
-# dateset di train
+# Train Dateset
 dataset_dict = convert_to_dict(train_data)
 dataset_train = Dataset.from_dict(dataset_dict)
 
-# dataset di validazione
+# Validation Dataset
 dataset_dict = convert_to_dict(validation_data)
 dataset_validation = Dataset.from_dict(dataset_dict)
 
 instruction = "Sei Unipa-GPT, il chatbot e assistente virtuale dell'Università degli Studi di Palermo.\nRispondi in modo cordiale e colloquiale alle domande fornite.\nSe ricevi un saluto, rispondi salutando e presentandoti.\nSe ricevi una domanda per quanto riguarda l'Università degli Studi di Palermo, rispondi facendo affidamento sulla documentazione che ti è stata consegnata insieme alla domanda.\nSe non sai rispondere, chiedi scusa e suggerisci di consultare il sito dell'Università [https://www.unipa.it/], non inventare risposte.\nRispondi in italiano.\n"
 
+# Chat Template
 def format_chat_template(row):
     row_json = [{"role": "user", "content": "Ciao"},
                 {"role": "assistant", "content": instruction},
@@ -76,10 +83,12 @@ tokenized_datasets_validation = tokenized_datasets_validation.remove_columns(['i
 tokenized_datasets_train = tokenized_datasets_train.rename_column('output', 'labels')
 tokenized_datasets_validation = tokenized_datasets_validation.rename_column('output', 'labels')
 
+# Global Dataset 
 tokenized_datasets = DatasetDict({"train": tokenized_datasets_train, "validation": tokenized_datasets_validation})
 
 from trl import SFTTrainer, SFTConfig
 
+# Training Args
 training_args = SFTConfig(
     output_dir="output_dir",
     num_train_epochs=30,
@@ -100,6 +109,7 @@ training_args = SFTConfig(
     packing=True
 ) 
 
+# Training with SFTTrainer
 trainer = SFTTrainer(
     model=model,
     tokenizer=tokenizer,
